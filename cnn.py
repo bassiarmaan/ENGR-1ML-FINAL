ecf999caa6b09acb208354297c984f85db80be50
asmik12 <asmikawatkar12@g.ucla.edu>
2025-02-28 16:29:36 -0800

    CNN testing and loading completed

--- cnn.py
+++ cnn.py
@@ -6,7 +6,6 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)  # Input: 1 channel, Output: 32 channels
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)  # Input: 32 channels, Output: 64 channels
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)  # Input: 64 channels, Output: 64 channels
         self.fc1 = nn.Linear(64 * 7 * 7, 64)  # Flattened input size: 64 * 7 * 7 (after 2 max-pool operations)
         self.fc2 = nn.Linear(64, 10)  # Output layer with 10 classes (0-9)
 
@@ -15,7 +14,6 @@ class CNN(nn.Module):
         x = torch.max_pool2d(x, 2)
         x = torch.relu(self.conv2(x))
         x = torch.max_pool2d(x, 2)
-        x = torch.relu(self.conv3(x))
         x = x.view(-1, 64 * 7 * 7)  # Flatten the tensor
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
